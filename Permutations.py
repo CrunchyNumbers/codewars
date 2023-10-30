3690a74305a06c75fa95dbf30fd9851f13003561
CrunchyNumbers <132238300+CrunchyNumbers@users.noreply.github.com>
2023-10-29 22:56:37 -0400

    Permutations.py

---
+++ Permutations.py
@@ -0,0 +1,44 @@
+'''
+In this kata, your task is to create all permutations of a non-empty input string and remove duplicates, if present.
+
+Create as many "shufflings" as you can!
+
+Examples:
+
+With input 'a':
+Your function should return: ['a']
+
+With input 'ab':
+Your function should return ['ab', 'ba']
+
+With input 'abc':
+Your function should return ['abc','acb','bac','bca','cab','cba']
+
+With input 'aabb':
+Your function should return ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+
+Note: The order of the permutations doesn't matter.
+
+Good luck!
+'''
+
+#Solution:
+
+def permutations(s):
+
+  # base case if the string length is less or equal to 1:
+  if len(s) <= 1:
+    return [s]
+
+  # initialization of result list for permutations
+  res = set()
+
+  # iterating through each character in the given string
+  for i, c in enumerate(s):
+    perm = permutations(s[:i] + s[i+1:])
+
+    # adding current char to the beginning of each permutation in the recursive result
+    for p in perm:
+      res.add(c + p)
+      
+  return list(res)
